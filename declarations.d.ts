3b61543c3a22b247311a2c4cccac3a12a674447c
Anh H Nguyen <iam@hoanganh.dev>
2023-03-24 23:27:07 -0400

    Remove declaratinos.d/ts

--- declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-  import * as React from 'react';
-
-  // Using React Component instead of React.FunctionComponent to avoid issue with missing defaultProps
-  const content: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  export default content;
-}
