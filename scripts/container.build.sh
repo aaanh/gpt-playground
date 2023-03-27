28a37a52175d8714dbb9c4a843d354f8072c1f34
Anh Hoang Nguyen <iam@hoanganh.dev>
2023-03-26 21:23:52 -0400

    Update build scrirpt

--- scripts/container.build.sh
+++ scripts/container.build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -f docker/next-base.Dockerfile -t aaanh.azurecr.io/web/next-base:latest && \
-docker build . -f docker/prod.Dockerfile -t aaanh.azurecr.io/gpt/frontend:latest
+docker build . -f docker/frontend.Dockerfile -t aaanh.azurecr.io/gpt/frontend:latest
