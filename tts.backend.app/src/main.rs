2ffd72d97e469b08cd4ad042dc05800be76f1a98
Anh H Nguyen <iam@hoanganh.dev>
2023-04-01 22:57:35 -0400

    Clean up project

--- tts.backend.app/src/main.rs
+++
@@ -1,9 +0,0 @@
-use warp::Filter;
-
-#[tokio::main]
-async fn main() {
-    let root = warp::path::end().map(|| "Hello, world!");
-    let routes = root.with(warp::cors().allow_any_origin());
-
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-}
